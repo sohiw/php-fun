875a0e5acf39237ad1bc063ad32ecd5c9d0fb510
Sohiw <sohiwdev@gmail.com>
2017-01-27 11:56:38 +0330

    Write admin.php

---
+++ admin.php
@@ -0,0 +1,9 @@
+<?php
+-- read and tech :|
+
+$admin = your id here;
+if ($admin == your id here) {
+print("Admin Has been set');
+SendMessage($admin, 'Welcome To Your New FuN TG robot\n\n\nPlease Send /help_admin First.');
+}
+?>
